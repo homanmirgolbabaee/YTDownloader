6bfb5418555b9ec90b9add6ac2fa6ff832a9591b
Homan Mirgolbabaee <homanmirgolbabaee@gmail.com>
2023-08-29 01:59:13 +0200

    youtube-downloader with a buggy UI

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -1,31 +1,98 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
 from pytube import YouTube
+from threading import Thread
 
-def download_video(url, output_path='.'):
-    try:
-        yt = YouTube(url)
-        video_stream = yt.streams.get_highest_resolution()
-        video_stream.download(output_path)
-        messagebox.showinfo("Download Complete", "Download completed!")
-    except Exception as e:
-        messagebox.showerror("Error", f"An error occurred: {str(e)}")
+class YouTubeDownloaderApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("YouTube Video Downloader")
+        self.root.geometry("400x300")
 
-def get_url():
-    url = url_entry.get()
-    download_video(url)
-    url_entry.delete(0, tk.END)  # Clear the entry field after downloading
+        self.create_widgets()
 
-root = tk.Tk()
-root.title("YouTube Video Downloader")
+    def create_widgets(self):
+        title_label = tk.Label(
+            self.root, text="Welcome to YouTube Video Downloader", font=("Helvetica", 16)
+        )
+        title_label.pack(pady=15)
 
-url_label = tk.Label(root, text="Enter YouTube Video URL:")
-url_label.pack()
+        self.url_entry = tk.Entry(self.root, width=40, font=("Helvetica", 12))
+        self.url_entry.pack()
 
-url_entry = tk.Entry(root, width=40)
-url_entry.pack()
+        self.download_button = tk.Button(
+            self.root,
+            text="Download Video",
+            command=self.start_download_thread,
+            bg="#FF5733",
+            fg="white",
+            font=("Helvetica", 12, "bold"),
+            relief=tk.FLAT,
+        )
+        self.download_button.pack(pady=10)
 
-download_button = tk.Button(root, text="Download", command=get_url)
-download_button.pack()
+        self.progress_bar = ttk.Progressbar(
+            self.root,
+            orient="horizontal",
+            length=300,
+            mode="determinate",
+            value=0,
+            maximum=100,
+        )
 
-root.mainloop()
+    def start_download_thread(self):
+        self.download_button.config(state=tk.DISABLED)
+        self.progress_bar.pack(pady=10)
+        
+        url = self.url_entry.get()
+        if url:
+            self.download_thread = Thread(target=self.download_video, args=(url,))
+            self.download_thread.start()
+        else:
+            messagebox.showwarning("Warning", "Please enter a valid YouTube URL")
+            self.download_button.config(state=tk.NORMAL)
+        
+    def update_progress_bar(self, stream, chunk, bytes_remaining):
+        total_size = stream.filesize
+        downloaded_size = total_size - bytes_remaining
+        progress = int((downloaded_size / total_size) * 100)
+        self.progress_bar["value"] = progress
+        self.root.update_idletasks()
+
+    def download_video(self, url):
+        try:
+            yt = YouTube(url)
+            video_stream = yt.streams.get_highest_resolution()
+            
+            self.show_loading_animation()  # Call the loading animation function
+
+            video_stream.download(output_path="downloads/")
+            messagebox.showinfo("Download Complete", "Download completed!")
+        except Exception as e:
+            messagebox.showerror("Error", f"An error occurred: {str(e)}")
+
+        self.progress_bar.pack_forget()
+        self.download_button.config(state=tk.NORMAL)
+
+    def show_loading_animation(self):
+        loading_label = tk.Label(self.root, text="Downloading...")
+        loading_label.pack()
+
+        def update_progress():
+            current_value = self.progress_bar["value"]
+            if current_value < 100:
+                self.progress_bar["value"] = current_value + 10
+                self.root.after(1000, update_progress)
+            else:
+                loading_label.config(text="Download Complete!")
+
+        update_progress()
+
+def main():
+    root = tk.Tk()
+    app = YouTubeDownloaderApp(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
