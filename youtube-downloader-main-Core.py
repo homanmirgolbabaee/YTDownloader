1ba0a3009bc47dbfde4d3fe1841494e205bc0816
Homan Mirgolbabaee <homanmirgolbabaee@gmail.com>
2023-08-29 19:44:04 +0200

    YT_Downloader Script UPDATED on Streamlit

--- youtube-downloader-main-Core.py
+++ youtube-downloader-main-Core.py
@@ -1,15 +1,27 @@
-
+import streamlit as st
 from pytube import YouTube
 
-def download_video(url, output_path='.'):
-    try:
-        yt = YouTube(url)
-        video_stream = yt.streams.get_highest_resolution()
-        video_stream.download(output_path)
-        print("Download completed!")
-    except Exception as e:
-        print("An error occurred:", str(e))
+def main():
+    st.title("YouTube Video Downloader")
+    st.write("Enter the YouTube video URL below:")
+   
+    video_url = st.text_input("Video URL:")
+    
+    if st.button("Download"):
+        try:
+            yt = YouTube(video_url)
+            st.write("Title:", yt.title)
+            st.write("Length:", yt.length)
+           
+            stream = yt.streams.get_highest_resolution()
+           
+            st.write("Downloading...")
+            stream.download(output_path='downloads')  # Change the output path if needed
+            st.success("Download complete!")
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    video_url = input("Enter the YouTube video URL: ")
-    download_video(video_url)
+    st.set_page_config(page_title="YouTube Downloader")
+
+    main()
