bda6c4f0f4350eeaae602423be9bb168e1fb4457
Homan Mirgolbabaee <homanmirgolbabaee@gmail.com>
2023-09-04 10:02:50 +0200

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,36 +1,54 @@
-
 from pytube import YouTube, exceptions as pytube_exceptions
 import streamlit as st
 
-st.title("Youtube Video Downloader")
-st.subheader("Enter the URL:")
-url = st.text_input(label='URL')
+# UI Header
+st.title("YouTube Video Downloader")
+st.write("This application allows you to download YouTube videos and audio.")
 
+# URL input
+st.subheader("Step 1: Enter the YouTube Video URL")
+url = st.text_input("YouTube URL", "")
+
+# Initialize variables
+yt = None
+video = None
+
+# URL validation and video information
 try:
-    yt = YouTube(url)
+    if url:
+        yt = YouTube(url)
+        st.subheader("Step 2: Video Information")
+        
+        # Display video details and thumbnail
+        col1, col2 = st.columns(2)
+        with col1:
+            st.image(yt.thumbnail_url, width=300)
+        with col2:
+            st.write(f"**Title:** {yt.title}")
+            st.write(f"**Length:** {yt.length} seconds")
+            st.write(f"**Rating:** {yt.rating}")
+            
+        video = yt.streams
 except pytube_exceptions.RegexMatchError:
-    st.subheader("Invalid YouTube URL. Please enter a valid URL.")
+    st.error("Invalid YouTube URL. Please enter a valid URL.")
 
-if url != '':
-    st.image(yt.thumbnail_url, width=300)
-    st.subheader('''
-    {}
-    ## Length: {} seconds
-    ## Rating: {} 
-    '''.format(yt.title, yt.length, yt.rating))
-    video = yt.streams
-    if len(video) > 0:
-        downloaded, download_audio = False, False
-        download_video = st.button("Download Video")
-        if yt.streams.filter(only_audio=True):
-            download_audio = st.button("Download Audio Only")
-        if download_video:
-            video.get_lowest_resolution().download()
-            downloaded = True
-        if download_audio:
-            video.filter(only_audio=True).first().download()
-            downloaded = True
-        if downloaded:
-            st.subheader("Download Complete")
-    else:
-        st.subheader("Sorry, this video can not be downloaded")
+# Download options
+if video and len(video) > 0:
+    st.subheader("Step 3: Download Options")
+    
+    # Select format
+    stream_options = ["Video", "Audio Only"]
+    selected_option = st.selectbox("Select Format", stream_options)
+    
+    # Download button
+    if st.button("Download"):
+        with st.spinner("Downloading..."):
+            if selected_option == "Video":
+                video.get_lowest_resolution().download()
+            else:
+                video.filter(only_audio=True).first().download()
+        
+        st.success("Download Complete 🎉")
+else:
+    if url:
+        st.warning("Sorry, this video cannot be downloaded.")
